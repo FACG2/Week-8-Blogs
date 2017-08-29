370970297acc0a23b3a3c9006560e5be90f3fb22
samer elaila <samer-elaila@live.com>
2017-08-29 15:40:14 +0300

    Change to uppercase and change date to timestamp

--- src/model/Database/db_build.sql
+++ src/model/Database/db_build.sql
@@ -11,10 +11,10 @@ CREATE TABLE admins (
 CREATE TABLE blogs (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(100) NOT NULL,
-  contents text NOT NULL,
+  contents TEXT NOT NULL,
   img_url VARCHAR(100),
-  blog_date date,
-  admin_id integer references admins(id)
+  blog_date TIMESTAMP,
+  admin_id INTEGER REFERENCES admins(id)
 );
 
 INSERT INTO admins (name, password) VALUES
@@ -24,9 +24,9 @@ INSERT INTO admins (name, password) VALUES
 ('samer', 'samer4');
 
 INSERT INTO blogs (title, contents, img_url, blog_date, admin_id) VALUES
-('blog1', 'Hello World','https://goo.gl/re4yz6', '2017-08-29',1),
-('blog2', 'Welcome', 'https://goo.gl/c8NkST', '2017-08-29',2),
-('blog3', '3eed Adha Mobarak','https://goo.gl/9VVikX', '2017-08-29',3),
-('blog4', 'Enjoy Your Time', 'https://goo.gl/Xod6nC', '2017-08-29',4);
+('blog1', 'Hello World','https://goo.gl/re4yz6', '2017-08-29 00:00:05',1),
+('blog2', 'Welcome', 'https://goo.gl/c8NkST', '2017-08-29 00:00:01',2),
+('blog3', '3eed Adha Mobarak','https://goo.gl/9VVikX', '2017-08-29 00:00:02',3),
+('blog4', 'Enjoy Your Time', 'https://goo.gl/Xod6nC', '2017-08-29 00:00:03',4);
 
 COMMIT ;
