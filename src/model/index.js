152d52047ea34c0b636606559bc9afe7b867c5a6
Salwa <herotony111@gmail.com>
2017-08-30 14:12:11 +0300

    add some postData queries

--- src/model/index.js
+++ src/model/index.js
@@ -1,5 +1,10 @@
 const blogQuery = require('./queries');
 
 module.exports = {
-  getAllBlogs: blogQuery.getAll
+  getAllBlogs: blogQuery.getAll,
+  getBlogById: blogQuery.getBlogById,
+  getAdmins: blogQuery.getAdmins,
+  postAddBlog: blogQuery.addBlog,
+  postDeleteBlog: blogQuery.deleteBlog,
+  postUpdateBlog: blogQuery.updateBlog
 };
