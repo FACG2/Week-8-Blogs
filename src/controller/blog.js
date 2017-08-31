8cf3b1038907f6869ced4f0696036229f1531afd
samer elaila <samer-elaila@live.com>
2017-08-31 10:41:22 +0300

    add error handling

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,40 +1,40 @@
 const {getBlogById, updateBlog, deleteBlog} = require('../model/queries');
 
-function get (req, res) {
+function get (req, res, next) {
   getBlogById(req.params.id, (err, blog) => {
     if (err) {
-      res.render('404');
+      next(err);
     } else {
       res.render('blog', {blog: blog[0], title: blog[0].title, cssPath: '/css/index.css'});
     }
   });
 }
 
-function getEditBlog (req, res) {
+function getEditBlog (req, res, next) {
   getBlogById(req.params.id, (err, blog) => {
     if (err) {
-      res.render('404');
+      next(err);
     } else {
       res.render('editBlog', {blog: blog[0], title: 'Edit Blog', cssPath: '/css/add_blog.css'});
     }
   });
 }
 
-function post (req, res) {
+function post (req, res, next) {
   const data = {
     body: req.body,
     params: req.params
   };
   updateBlog(data, (err, result) => {
     if (err) {
-      res.redirect('/404');
+      next(err);
     } else {
       res.redirect('/blogs/' + req.params.id);
     }
   });
 }
 
-function deleteBlogById (req, res) {
+function deleteBlogById (req, res, next) {
   const data = {
     body: req.body,
     params: req.params
@@ -42,7 +42,7 @@ function deleteBlogById (req, res) {
 
   deleteBlog(data, (err, result) => {
     if (err) {
-      res.redirect('/404');
+      next(err);
     } else {
       res.redirect('/admin');
     }
